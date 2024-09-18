284e2afed05845c203ccd7f2003855e90f2c9c71
Ginhoor <ginhoor@gmail.com>
2024-09-18 17:06:05 +0800

    update

---
+++ build.sh
@@ -0,0 +1,1 @@
+docker build -t feishu_robot .
---
+++ run.sh
@@ -0,0 +1,6 @@
+docker run --restart=always --name douyin_download \
+-v /root/workspace/DouyinLiveRecorder/config:/app/config \
+-v /root/workspace/DouyinLiveRecorder/logs:/app/logs \
+-v /root/workspace/DouyinLiveRecorder/backup_config:/app/backup_config \
+-v /mnt/data_wd_700/douyin_downloads:/app/downloads \
+-d douyin_recorder
