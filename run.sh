b3fba0a53574628efcdaf5d7db9cd6aaae38c71c
Ginhoor <ginhoor@gmail.com>
2024-09-19 08:41:07 +0800

    增加环境变量

--- run.sh
+++ run.sh
@@ -3,4 +3,5 @@ docker run --restart=always --name douyin_download \
 -v /root/workspace/DouyinLiveRecorder/logs:/app/logs \
 -v /root/workspace/DouyinLiveRecorder/backup_config:/app/backup_config \
 -v /mnt/data_wd_700/douyin_downloads:/app/downloads \
+-e TERM=xterm-256color \
 -d douyin_recorder
